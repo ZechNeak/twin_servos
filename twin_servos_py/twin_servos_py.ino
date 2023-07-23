488a5b07448f4bc7476c68b1caba5b0cba7e5778
Zechariah Neak <zack.neak@gmail.com>
2023-07-23 02:22:52 -0700

    Can now turn both motors with a PWM signal viu Pyduino

--- twin_servos_py/twin_servos_py.ino
+++ twin_servos_py/twin_servos_py.ino
@@ -64,6 +64,7 @@ void loop() {
   // TODO: Create an alphabetically sorted array of string-function pairs and use binary search instead
   if (strcmp(commandInput, "pos") == 0) showCurrentPositions();
   else if (strcmp(commandInput, "servo") == 0) switchServo(argInputs[0]);
+  else if (strcmp(commandInput, "pwm") == 0) sendPwmSignal(argInputs[0]);
 
   // Prevents Arduino from continuously executing the last command received
   strcpy(ardBridge.headerOfMsg, "xyz");
@@ -104,5 +105,90 @@ void switchServo(int servo) {
     Serial.print("<STATUS - Set servo: ");
     if (currentServo == SERVO_X) Serial.println("X>");
     else Serial.println("Y>");
+  }
+}
+
+
+/*
+ *  If target_pwm is below 600usec, or exceeds 1500usec, then convert it to another
+ *  value that retains the same position in degrees. This is achieved by wrapping the
+ *  value around the range 600-1500. Negative numbers, or much larger numbers, may
+ *  require multiple wrapping operations.
+ *
+ *  Ex: Both 1650 and 750usec roughly correspond to the 60-degree angle, but 750 usec
+ *      retains more physical precision. The alternative can be off by a few degrees.
+ */
+int normalize_pwm(int target_pwm) {
+  while (target_pwm < PWM_MIN) {
+    target_pwm = PWM_FULL_REV - abs(PWM_MIN - target_pwm);
+  }
+  while (PWM_FULL_REV < target_pwm) {
+    target_pwm = PWM_MIN + abs(target_pwm - PWM_FULL_REV);
+  }
+
+  if ((PWM_MIN <= target_pwm) && (target_pwm <= PWM_FULL_REV)) return target_pwm;
+
+  // TODO: else {} halt for unexpected value
+}
+
+
+/* 'pwm'
+ *
+ *  Turns servo towards angle position mapped to the given PWM period value
+ */
+void sendPwmSignal(int target_pwm) {
+
+  // Adjust the PWM signal while retaining the same destination position
+  if (target_pwm < PWM_MIN || PWM_FULL_REV < target_pwm) {
+    target_pwm = normalize_pwm(target_pwm);
+  }
+
+  // Convert PWM period to degrees
+  int target_pos = map(target_pwm, PWM_MIN, PWM_FULL_REV, 0, 360);
+
+  if (currentServo == 0) Serial.print("<Servo X");
+  else if (currentServo == 1) Serial.print("<Servo Y");
+  Serial.print(" turning to ");
+  Serial.print(String(target_pos));
+  Serial.println(" degrees>");
+
+  // Travel towards the target angle in steps of 5
+  int curr_pwm;
+  int curr_pos;
+  // TODO: Declare Servo pointer here, instead of using all the conditionals below.
+
+  if (currentServo == SERVO_X) curr_pwm = curr_pwm_x;
+  else curr_pwm = curr_pwm_y;
+
+  int sig = curr_pwm;
+
+  if (curr_pwm <= target_pwm) {
+    while (sig <= target_pwm) {
+      if (currentServo == SERVO_X) servoX.writeMicroseconds(sig);
+      else servoY.writeMicroseconds(sig);
+      // Serial.println(sig);
+      delay(15);
+      sig += 5;
+    }
+  }
+  else { //turn backwards
+    while (sig >= target_pwm) {
+      if (currentServo == SERVO_X) servoX.writeMicroseconds(sig);
+      else servoY.writeMicroseconds(sig);
+      // Serial.println(sig);
+      delay(15);
+      sig -= 5;
+    }
+  }
+  // Final push to ensure accuracy
+  if (currentServo == SERVO_X) {
+    servoX.writeMicroseconds(target_pwm);
+    curr_pwm_x = target_pwm;
+    curr_pos_x = target_pos;
+  }
+  else {
+    servoY.writeMicroseconds(target_pwm);
+    curr_pwm_y = target_pwm;
+    curr_pos_y = target_pos;
   }
 }
