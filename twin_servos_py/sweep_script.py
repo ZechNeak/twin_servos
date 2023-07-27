fa8ed7bd4066fbd72a5e20d518406f52155a6305
Zechariah Neak <zack.neak@gmail.com>
2023-07-27 00:58:43 -0700

    Added comment block and minor tweaks.

--- twin_servos_py/sweep_script.py
+++ twin_servos_py/sweep_script.py
@@ -1,11 +1,37 @@
-# This is meant to interface with an Arduino Uno over a serial port.
-# Heavily depends on the PyDuinoBridge library.
-#
-# Instead of allowing for command input, this is simply a script 
-# that begins a full sweep of both servos immediately after the
-# servos and Arduino are ready.
-#
-# Companion file: twin_servos_py.ino
+"""
+======================================================================
+This is meant to interface with an Arduino Uno over a serial port.
+Heavily depends on the PyDuinoBridge library.
+
+Instead of allowing for command input, this is simply a script
+that begins a full sweep of both servos immediately after the
+servos and Arduino are ready.
+
+    *** Requirement: pip install pyduinobridge ***
+
+
+Messages to the Arduino must be in this string format:
+
+    "<[command],[# of degrees]>"
+
+    EX: writeAndRead_Strings(["<ymove," + str(y_sweep/2) + ">"])
+
+
+Available Commands:
+
+    xmove, ymove   =   Switch to Servo X or Y, and move from current
+                      position by a # of degrees.
+
+    xgoto, ygoto   =   Switch to Servo X or Y, and move to a specified
+                      absolute position in degrees.
+
+    move, goto     =   Perform a "move" or "goto" while on the
+                      currently selected Servo.
+
+
+Companion file: twin_servos_py.ino
+======================================================================
+"""
 
 from pyduinobridge import Bridge_py
 
@@ -15,8 +41,9 @@ baudRate = 115200
 ardBridge = Bridge_py()
 ardBridge.begin(serPort, baudRate, numIntValues_FromPy=1, numFloatValues_FromPy=0)
 # ardBridge.setVerbosity(0)
-ardBridge.setSleepTime(2)   # Set this to like 7sec later
+ardBridge.setSleepTime(3)
 
+# Edit these values as desired
 x_pos = 180
 x_step = 2
 forward_flag = True
@@ -25,9 +52,9 @@ y_sweep = 80
 x_bound_min = 180 - (x_sweep/2)
 x_bound_max = 180 + (x_sweep/2)
 
-# Initialize both servos back to their origins
-ardResponse = ardBridge.writeAndRead_Strings(["<xgoto,180>"])
-ardResponse = ardBridge.writeAndRead_Strings(["<ygoto,180>"])
+# Initialize both servos back to their origins (likely redundant)
+# ardResponse = ardBridge.writeAndRead_Strings(["<xgoto,180>"])
+# ardResponse = ardBridge.writeAndRead_Strings(["<ygoto,180>"])
 print("++++++++++++++++++++++++++++++++++++++")
 
 # Start by sweeping Y from the origin
